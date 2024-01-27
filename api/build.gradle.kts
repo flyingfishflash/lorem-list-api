3d9a9160e3d0302a5e21f3c39ff612d1b01d2c10
flyingfishflash <flash@flyingfishflash.net>
2024-01-27 14:48:20 -0500

    refactor: @ReponseBody for method not returning explicit ResponseEntity

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
   id("io.spring.dependency-management") version "1.1.4"
 //  id("org.graalvm.buildtools.native") version "0.9.28"
   id("org.springframework.boot") version "3.2.2"
+  id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
   kotlin("jvm") version "1.9.22"
   kotlin("plugin.jpa") version "1.9.22"
   kotlin("plugin.serialization") version "1.9.22"
