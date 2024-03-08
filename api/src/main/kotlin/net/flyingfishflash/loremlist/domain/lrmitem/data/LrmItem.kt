869d5ee3816923da5da2f1771f80aab3c23233e7
flyingfishflash <flash@flyingfishflash.net>
2024-03-08 14:27:08 -0500

    refactor: lrmitem quantity serialize default value

--- api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmitem/data/LrmItem.kt
+++ api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmitem/data/LrmItem.kt
@@ -1,16 +1,19 @@
 package net.flyingfishflash.loremlist.domain.lrmitem.data
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import net.flyingfishflash.loremlist.domain.lrmlist.data.LrmListSuccinct
 
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
 data class LrmItem(
   var id: Long,
   var created: Instant? = null,
   var name: String,
   var description: String? = null,
-  var quantity: Long = 0,
+  @EncodeDefault var quantity: Long = 0,
   val lists: Set<LrmListSuccinct>? = null,
 ) {
   fun copyWith(
