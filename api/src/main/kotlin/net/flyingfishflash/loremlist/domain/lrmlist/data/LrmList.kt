77a82726098b6c0d68e99b4c015c53517196b644
flyingfishflash <flash@flyingfishflash.net>
2024-03-08 14:32:36 -0500

    refactor: clean-up

--- api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmlist/data/LrmList.kt
+++ api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmlist/data/LrmList.kt
@@ -11,35 +11,7 @@ data class LrmList(
   var name: String,
   var description: String? = null,
   val items: Set<LrmItem>? = null,
-) {
-//  fun copyWith(
-//    id: Long = this.id,
-//    created: Instant? = this.created,
-//    name: String = this.name,
-//    description: String? = this.description,
-//    items: Set<LrmItem>? = null,
-//  ): LrmList {
-//    return LrmList(id, created, name, description, items)
-//  }
-}
-
-// class LrmListWithItems(
-//  id: Long,
-//  created: Instant? = null,
-//  name: String,
-//  description: String? = null,
-//  val items: Set<LrmItem>? = null // Replace List<String> with the actual type of your items
-// ) : LrmList(id, created, name, description) {
-//  fun copyWith(
-//    id: Long = this.id,
-//    created: Instant? = this.created,
-//    name: String = this.name,
-//    description: String? = this.description,
-//    items: Set<LrmItem>? = this.items
-//  ): LrmListWithItems {
-//    return LrmListWithItems(id, created, name, description, items)
-//  }
-// }
+)
 
 @Serializable
 data class LrmListSuccinct(
--- api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmlist/data/LrmListConverter.kt
+++ api/src/main/kotlin/net/flyingfishflash/loremlist/domain/lrmlist/data/LrmListConverter.kt
@@ -1,26 +1,8 @@
 package net.flyingfishflash.loremlist.domain.lrmlist.data
 
-import net.flyingfishflash.loremlist.domain.LrmListTable
 import net.flyingfishflash.loremlist.domain.lrmlist.data.dto.LrmListRequest
-import org.jetbrains.exposed.sql.ResultRow
 
 object LrmListConverter {
-  fun toLrmList(resultRow: ResultRow): LrmList =
-    LrmList(
-      id = resultRow[LrmListTable.id].value,
-      created = resultRow[LrmListTable.created],
-      name = resultRow[LrmListTable.name],
-      description = resultRow[LrmListTable.description],
-    )
-
-  fun toLrmListAndItems(resultRow: ResultRow): LrmList =
-    LrmList(
-      id = resultRow[LrmListTable.id].value,
-      created = resultRow[LrmListTable.created],
-      name = resultRow[LrmListTable.name],
-      description = resultRow[LrmListTable.description],
-    )
-
   fun toLrmList(
     lrmListRequest: LrmListRequest,
     lrmList: LrmList,
