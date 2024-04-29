f7c4c3bee9df02c4a5ab61a4e1661327d1750273
flyingfishflash <flash@flyingfishflash.net>
2024-04-28 22:40:12 -0400

    refactor: core error logging improvements
    - warn for client error (400)
    - error for server error (500)
    - log type and value of unexamined object in custom response body advice

--- api/src/main/kotlin/net/flyingfishflash/loremlist/core/response/advice/ApiExceptionHandler.kt
+++ api/src/main/kotlin/net/flyingfishflash/loremlist/core/response/advice/ApiExceptionHandler.kt
@@ -2,8 +2,6 @@ package net.flyingfishflash.loremlist.core.response.advice
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.servlet.http.HttpServletRequest
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import net.flyingfishflash.loremlist.core.exceptions.AbstractApiException
 import net.flyingfishflash.loremlist.core.response.structure.ApiProblemDetail
 import net.flyingfishflash.loremlist.core.response.structure.ResponseProblem
@@ -26,9 +24,9 @@ class ApiExceptionHandler {
       status = exception.httpStatus.value(),
       detail = exception.detail,
     )
-    // TODO initialize ApiProblemDetail extension properties either here or in in ApiProblemDetail constructor
+
     val applicationResponse = ResponseProblem(apiProblemDetail, exception.responseMessage, request)
-    log.info { Json.encodeToString(applicationResponse) }
+
     return ResponseEntity<ResponseProblem?>(applicationResponse, exception.httpStatus)
   }
 }
--- api/src/main/kotlin/net/flyingfishflash/loremlist/core/response/advice/CustomResponseBodyAdvice.kt
+++ api/src/main/kotlin/net/flyingfishflash/loremlist/core/response/advice/CustomResponseBodyAdvice.kt
@@ -18,10 +18,25 @@ import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+import java.util.UUID
 
 @RestControllerAdvice
 class CustomResponseBodyAdvice : ResponseBodyAdvice<Any?> {
   private val logger = KotlinLogging.logger {}
+
+  private fun logResponseProblem(applicationResponse: ResponseProblem) {
+    when {
+      HttpStatus.valueOf(applicationResponse.content.status).is5xxServerError -> {
+        logger.error { Json.encodeToString(applicationResponse) }
+      }
+      HttpStatus.valueOf(applicationResponse.content.status).is4xxClientError -> {
+        logger.warn { Json.encodeToString(applicationResponse) }
+      }
+      else -> logger.info {
+        Json.encodeToString(applicationResponse)
+      }
+    }
+  }
 
   override fun supports(returnType: MethodParameter, converterType: Class<out HttpMessageConverter<*>>): Boolean {
     // exclude swagger from beforeBodyWrite
@@ -41,24 +56,32 @@ class CustomResponseBodyAdvice : ResponseBodyAdvice<Any?> {
     var applicationResponse: Response<*>? = null
 
     when {
-      o is Response<*> -> {
+      o is ResponseSuccess<*> -> {
         applicationResponse = o
+      }
+      // --
+      o is ResponseProblem -> {
+        applicationResponse = o
+        logResponseProblem(applicationResponse)
       }
       // --
       o is Throwable && o !is ErrorResponseException -> {
         applicationResponse = ResponseProblem(ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR), serverHttpRequest)
-        logger.info { Json.encodeToString(applicationResponse) }
+        logger.warn { "o is Throwable && o !is ErrorResponseException" }
+        logResponseProblem(applicationResponse)
       }
       // --
       o is ErrorResponseException -> {
         applicationResponse = ResponseProblem(problemDetail = o.body, serverHttpRequest)
-        logger.info { Json.encodeToString(applicationResponse) }
+        logger.warn { "o is ErrorResponseException" }
+        logResponseProblem(applicationResponse)
       }
       // --
       o is ProblemDetail -> {
         val responseProblem = ResponseProblem(problemDetail = o, serverHttpRequest)
         applicationResponse = responseProblem
-        logger.info { Json.encodeToString(responseProblem) }
+        logger.warn { "o is ProblemDetail" }
+        logResponseProblem(applicationResponse)
       }
       // --
       methodParameter.containingClass.isAnnotationPresent(RestController::class.java) &&
@@ -80,7 +103,10 @@ class CustomResponseBodyAdvice : ResponseBodyAdvice<Any?> {
     }
 
     return applicationResponse ?: o.also {
-      logger.warn { "Returning object from CustomResponseBodyAdvice.beforeBodyWrite() without examination: $it" }
+      val id = UUID.randomUUID()
+      logger.warn { "[$id] Returning object from CustomResponseBodyAdvice.beforeBodyWrite() without examination" }
+      logger.warn { "[$id] type: ${it?.javaClass}" }
+      logger.warn { "[$id] value: $it" }
     }
   }
 }
