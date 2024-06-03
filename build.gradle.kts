c251595458b3e8e1fc8de145b5fde75b6522b4ab
flyingfishflash <flash@flyingfishflash.net>
2024-06-02 22:00:17 -0400

    dependency: kotlin-exposed 0.51.0 (#38)

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ val ciCommit by extra { ciCommit() }
 val ciPlatform by extra { ciPlatform() }
 val ciPipelineId by extra { ciPipelineId() }
 
-val exposedVersion = "0.50.1"
+val exposedVersion = "0.51.0"
 val flywayVersion = "10.13.0"
 val jakartaValidationApiVersion = "3.1.0"
 val kotestVersion = "5.9.0"
