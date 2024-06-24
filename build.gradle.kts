939b42be7a0d70c61e95d774932612aa9f4bb9be
flyingfishflash <flash@flyingfishflash.net>
2024-06-23 20:00:16 -0400

    dependency: spring-boot 3.3.1 (#55)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
   id("com.github.ben-manes.versions") version "0.51.0"
   id("io.spring.dependency-management") version "1.1.5"
   id("org.sonarqube") version "5.0.0.4638"
-  id("org.springframework.boot") version "3.3.0"
+  id("org.springframework.boot") version "3.3.1"
   id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
   kotlin("jvm") version "2.0.0"
   kotlin("plugin.jpa") version "2.0.0"
