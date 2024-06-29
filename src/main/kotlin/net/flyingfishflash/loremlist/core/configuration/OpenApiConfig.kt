b049cb7cd7fab85e76ac619a7cff44ee126c4dfe
flyingfishflash <flash@flyingfishflash.net>
2024-06-29 09:49:01 -0400

    refactor: open api meta data title, description, contact (#59)

--- src/main/kotlin/net/flyingfishflash/loremlist/core/configuration/OpenApiConfig.kt
+++ src/main/kotlin/net/flyingfishflash/loremlist/core/configuration/OpenApiConfig.kt
@@ -1,13 +1,33 @@
 package net.flyingfishflash.loremlist.core.configuration
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.media.Schema
 import kotlinx.datetime.Clock.System.now
 import org.springdoc.core.customizers.OpenApiCustomizer
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig {
+class OpenApiConfig(private val buildProperties: BuildProperties) {
+
+  @Bean
+  fun customOpenAPI(): OpenAPI {
+    return OpenAPI().info(
+      Info()
+        .contact(
+          Contact()
+            .name("flyingfishflash")
+            .url("https://codeberg.org/lorem-list/"),
+        )
+        .description("Simple List Management API")
+        .title("Lorem List Api")
+        .version(buildProperties.version),
+    )
+  }
+
   @Bean
   fun sortSchemasAlphabetically(): OpenApiCustomizer {
     return OpenApiCustomizer { openApi ->
