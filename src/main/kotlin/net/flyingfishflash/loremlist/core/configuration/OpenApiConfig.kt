951caf91d97e39b6ff4f49bf24dfca42c96f4298
flyingfishflash <flash@flyingfishflash.net>
2024-08-19 11:28:41 -0400

    refactor: swagger-ui configuration for oidc auth code + pkce

--- src/main/kotlin/net/flyingfishflash/loremlist/core/configuration/OpenApiConfig.kt
+++ src/main/kotlin/net/flyingfishflash/loremlist/core/configuration/OpenApiConfig.kt
@@ -1,9 +1,15 @@
 package net.flyingfishflash.loremlist.core.configuration
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import jakarta.servlet.ServletContext
 import kotlinx.datetime.Clock.System.now
@@ -16,8 +22,11 @@ import org.springframework.context.annotation.Configuration
 class OpenApiConfig(private val buildProperties: BuildProperties) {
 
   @Bean
-  fun customOpenAPI(servletContext: ServletContext): OpenAPI {
+  fun openAPI(servletContext: ServletContext): OpenAPI {
     return OpenAPI()
+      .components(
+        Components().addSecuritySchemes("Zitadel OIDC", createOAuthScheme()),
+      ).addSecurityItem(SecurityRequirement().addList("Zitadel OIDC"))
       .servers(listOf(Server().url(servletContext.contextPath)))
       .info(
         Info()
@@ -29,6 +38,30 @@ class OpenApiConfig(private val buildProperties: BuildProperties) {
           .description("List Management API")
           .title("Lorem List Api")
           .version(buildProperties.version),
+      )
+  }
+
+  private fun createOAuthScheme(): SecurityScheme {
+    val flows: OAuthFlows = createOAuthFlows()
+    return SecurityScheme().type(SecurityScheme.Type.OAUTH2)
+      .flows(flows)
+  }
+
+  private fun createOAuthFlows(): OAuthFlows {
+    val flow: OAuthFlow = createAuthorizationCodeFlow()
+    return OAuthFlows().authorizationCode(flow)
+  }
+
+  private fun createAuthorizationCodeFlow(): OAuthFlow {
+    return OAuthFlow()
+      .authorizationUrl("https://zitadel.flyingfishflash.net/oauth/v2/authorize")
+      .tokenUrl("https://zitadel.flyingfishflash.net/oauth/v2/token")
+      .scopes(
+        Scopes()
+          .addString("openid", "")
+          .addString("email", "")
+          .addString("profile", "")
+          .addString("offline_access", ""),
       )
   }
 
