26bbf0b4cd6a9d79a51fc606a16a5571875530a4
flyingfishflash <flash@flyingfishflash.net>
2025-02-09 09:55:47 -0500

    refactor: remove serialization annotations from association domain class

--- src/main/kotlin/net/flyingfishflash/loremlist/domain/association/Association.kt
+++ src/main/kotlin/net/flyingfishflash/loremlist/domain/association/Association.kt
@@ -1,15 +1,5 @@
 package net.flyingfishflash.loremlist.domain.association
 
-import kotlinx.serialization.Serializable
-import net.flyingfishflash.loremlist.core.serialization.UUIDSerializer
 import java.util.UUID
 
-@Serializable
-data class Association(
-  @Serializable(with = UUIDSerializer::class)
-  val id: UUID,
-  @Serializable(with = UUIDSerializer::class)
-  val itemId: UUID,
-  @Serializable(with = UUIDSerializer::class)
-  val listId: UUID,
-)
+data class Association(val id: UUID, val itemId: UUID, val listId: UUID)
