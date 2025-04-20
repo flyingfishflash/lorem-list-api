b07226f007184e51223d7fcf1c4b4f1fb4717b49
flyingfishflash <flash@flyingfishflash.net>
2025-04-20 10:40:48 -0400

    build: sonarqube.gradle.plugin [6.0.1.5171 -> 6.1.0.5360]

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
   id("com.diffplug.spotless") version "7.0.3"
   id("com.github.ben-manes.versions") version "0.52.0"
   id("io.spring.dependency-management") version "1.1.7"
-  id("org.sonarqube") version "6.0.1.5171"
+  id("org.sonarqube") version "6.1.0.5360"
   id("org.springframework.boot") version "3.4.3"
   id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
   kotlin("jvm") version "2.1.0"
